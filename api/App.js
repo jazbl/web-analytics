d7159988b2bcc486e00a9940634fe3105e40248d
Jasmine-Ball <jasmine.ball@uon.edu.au>
2021-05-01 23:11:56 +1000

    Update App.js Node/Express file to work with home page

--- api/App.js
+++ api/App.js
@@ -2,9 +2,18 @@ const express = require('express');
 const exp = express();
 const port = process.env.PORT || 8000;
 
+
+exp.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
 exp.get('/api', (req, res) => {
     
 res.send('Express App sending a res from the Express get OH');
 });
 
 exp.listen();
+
+
+
+
