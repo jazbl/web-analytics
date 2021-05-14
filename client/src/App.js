5a0518ca3767393bc2a04d301ebd199ecee752b5
Jasmine-Ball <jasmine.ball@uon.edu.au>
2021-05-15 00:12:19 +1000

    Add basic JSX to App.js file.

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,47 @@ import './App.css';
 function App() {
 
 const [state, setState] = useState({});
-const handleChange = () => {};
-const submit = () => {
-  axios({});
+
+
+const handleChange = ({ target }) => {
+  setState({[target.name]: target.value});
 };
+const submit = (e) => {
+  e.preventDefault();
+
+  axios({
+    url: 'api/save',
+    method: 'POST',
+    data: {title: state.title}
+  })
+  .then( ()=> {
+axios.get('/api')
+.then((res) => {
+  const data = res.data;
+  setState({ posts: data});
+})
+  });
+};
+
+// Could create a notification saying "Website not yet registered with Pagestats".
 
   return (
     <>
+  Analytics
+    <form onSubmit={submit}> 
+
+      <input
+        type="text"
+        name="title"
+        placeholder="Enter Website URL"
+        value={state.title}
+        onChange={handleChange}
+      />
+
+      <button>Show Statistics</button>
+
+    </form>
+
     </>
   );
 }
