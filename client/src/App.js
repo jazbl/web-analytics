80c0e1cb7c8c024c24a3f2781e7a926398ce2042
Jasmine-Ball <jasmine.ball@uon.edu.au>
2021-05-14 22:09:35 +1000

    Add Axios functions.

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+const [state, setState] = useState({});
+const handleChange = () => {};
+const submit = () => {
+  axios({});
+};
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
